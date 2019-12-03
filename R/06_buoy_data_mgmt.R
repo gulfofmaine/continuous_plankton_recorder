06c68d34f7c9b59e81633082298dfc47d08715b1
Adam A. Kemberling <adamkemberling@gmail.com>
2019-12-03 10:23:25 -0500

    Added stratification index to buoy data

--- R/06_buoy_data_mgmt.R
+++ R/06_buoy_data_mgmt.R
@@ -68,12 +68,15 @@ buoys <- buoys_collapsed %>%
   pivot_wider(names_from = var_name, values_from = daily_mean)
 
 #clean environment
-rm(Buoys, buoy_b, buoys_collapsed)
+rm(Buoys, buoy_b)
 
+
+
+#####  Estimate  Aggreagate Values  ####
 yearly_means <- buoys %>% 
   mutate(year = lubridate::year(Date),
          year = factor(year)) %>% 
-  group_by(buoy_id, year) %>% 
+  group_by(buoy_id, year, reading_depth) %>% 
   summarise(mean_temp = mean(temp, na.rm = T),
             mean_sal = mean(sal, na.rm = T),
             mean_dens = mean(density, na.rm = T)) %>% 
@@ -91,7 +94,7 @@ quarterly_means <- buoys %>%
            month_col %in% c(7:9)   ~ "Q3",
            month_col %in% c(10:12) ~ "Q4"
          )) %>% 
-  group_by(buoy_id, year, period) %>% 
+  group_by(buoy_id, year, period, reading_depth) %>% 
   summarise(mean_temp = mean(temp, na.rm = T),
             mean_sal = mean(sal, na.rm = T),
             mean_dens = mean(density, na.rm = T)) %>% 
@@ -110,7 +113,7 @@ bi_monthly_means <- buoys %>%
            month_col %in% c(9:10)   ~ "P5",
            month_col %in% c(11:12)   ~ "P6",
          )) %>% 
-  group_by(buoy_id, year, period) %>% 
+  group_by(buoy_id, year, period, reading_depth) %>% 
   summarise(mean_temp = mean(temp, na.rm = T),
             mean_sal = mean(sal, na.rm = T),
             mean_dens = mean(density, na.rm = T)) %>% 
@@ -119,4 +122,76 @@ bi_monthly_means <- buoys %>%
 #Put them back together
 buoy_dataset <- full_join(yearly_means, quarterly_means) %>% 
   full_join(bi_monthly_means)
-write_csv(buoy_dataset, path = str_c(cpr_boxpath, "data/processed_data/buoys_aggregated.csv", sep = "/"))
+
+
+####  Stratification index  ####
+# Brunt-Vaisala frequency N = sqrt(-g/potential density * delta density/delta depth)
+# the ocean stratification is quantified by the measured value of delta density/denta depth
+# If the water is more stratified, the frequency is higher. If less stratified, frequency is lower
+strat_set <- buoys_collapsed %>% 
+  filter(var_name == "density",
+         reading_depth %in% c("1 meter", "50 meters")) %>% 
+         pivot_wider(names_from = reading_depth, values_from = daily_mean) %>% 
+  split(.$buoy_id) %>% 
+  map(~ .x %>% mutate(
+    difference =  `1 meter` - `50 meters`,
+    index      = sqrt(-(difference)/50)
+  )) %>% 
+  bind_rows()
+
+
+#####  Estimate Aggregate Stratification Values  ####
+yearly_strat <- strat_set %>% 
+  mutate(year = lubridate::year(Date),
+         year = factor(year)) %>% 
+  group_by(buoy_id, year) %>% 
+  summarise(mean_diff = mean(difference, na.rm = T),
+            mean_strat_index = mean(index, na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(period = "Annual")
+
+quarterly_strat <- strat_set %>% 
+  mutate(year = lubridate::year(Date),
+         year = factor(year),
+         month_col = lubridate::month(Date),
+         month_col = factor(month_col),
+         period = case_when(
+           month_col %in% c(1:3)   ~ "Q1",
+           month_col %in% c(4:6)   ~ "Q2",
+           month_col %in% c(7:9)   ~ "Q3",
+           month_col %in% c(10:12) ~ "Q4"
+         )) %>% 
+  group_by(buoy_id, year, period) %>% 
+  summarise(mean_diff = mean(difference, na.rm = T),
+            mean_strat_index = mean(index, na.rm = T)) %>% 
+  ungroup()
+
+bi_monthly_strat <- strat_set %>% 
+  mutate(year      = lubridate::year(Date),
+         year      = factor(year),
+         month_col = lubridate::month(Date),
+         month_col = factor(month_col),
+         period    = case_when(
+           month_col %in% c(1:2)   ~ "P1",
+           month_col %in% c(3:4)   ~ "P2",
+           month_col %in% c(5:6)   ~ "P3",
+           month_col %in% c(7:8)   ~ "P4",
+           month_col %in% c(9:10)   ~ "P5",
+           month_col %in% c(11:12)   ~ "P6",
+         )) %>% 
+  group_by(buoy_id, year, period) %>% 
+  summarise(mean_diff = mean(difference, na.rm = T),
+            mean_strat_index = mean(index, na.rm = T)) %>% 
+  ungroup()
+
+
+strat_aggregates <- full_join(yearly_strat, quarterly_strat) %>% 
+  full_join(bi_monthly_strat)
+
+####  Combine with physical measurements  ####
+buoys_out <- full_join(buoy_dataset, strat_aggregates)
+
+
+
+####  Export Out  ####
+write_csv(buoys_out, path = str_c(cpr_boxpath, "data/processed_data/buoys_aggregated.csv", sep = "/"))
